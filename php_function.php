4c41cd7e6d2ea101c12cf6b7b3ebe80f72ea1c65
liuliming0214 <277631488@qq.com>
2017-06-22 10:53:54 +0800

    Create php_function.php

---
+++ php_function.php
@@ -0,0 +1,33 @@
+ function http_post_data($url, $data_string) {  
+  
+        $ch = curl_init();  
+        curl_setopt($ch, CURLOPT_POST, 1);  
+        curl_setopt($ch, CURLOPT_URL, $url);  
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);  
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
+            'Content-Type: application/json; charset=utf-8',  
+            'Content-Length: ' . strlen($data_string))  
+        );  
+        ob_start();  
+        curl_exec($ch);  
+        $return_content = ob_get_contents();  
+        ob_end_clean();  
+  
+        $return_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);  
+        return array($return_code, $return_content);  
+    }  
+
+function init_curl($url,$data=array())
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    $res = curl_exec ( $ch );
+    curl_close ( $ch );
+    $res = json_decode($res,true);
+    return $res;
+}
+
